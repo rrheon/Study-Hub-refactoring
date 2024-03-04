99a2013d7c820d8551ab9282f82c93a03f9fdd9b
rrheon <yongheon0806@gmail.com>
2024-03-05 00:13:45 +0900

    refactor - 로그인 과정 수정

--- STUDYHUB2/SceneDelegate.swift
+++ STUDYHUB2/SceneDelegate.swift
@@ -16,6 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
+    
    lazy var screenCheck: Bool = false
     loginManager.refreshAccessToken { result in
       DispatchQueue.main.async {
